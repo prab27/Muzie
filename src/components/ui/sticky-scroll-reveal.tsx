fc0cea140e5d81c23193b663004aefcc8263a95d
prab27 <ayushsin72@gmail.com>
2024-04-11 00:51:13 +0530

    update

---
+++ src/components/ui/sticky-scroll-reveal.tsx
@@ -0,0 +1,84 @@
+import React, { useRef } from "react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
+
+export const StickyScroll = ({
+  content,
+}: {
+  content: {
+    title: string;
+    description: string;
+  }[];
+}) => {
+  const [activeCard, setActiveCard] = React.useState(0);
+  const ref = useRef<any>(null);
+  const { scrollYProgress } = useScroll({
+    container: ref,
+    offset: ["start start", "end start"],
+  });
+  const cardLength = content.length;
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    const cardsBreakpoints = content.map((_, index) => index / cardLength);
+    cardsBreakpoints.forEach((breakpoint, index) => {
+      if (latest > breakpoint - 0.2 && latest <= breakpoint) {
+        setActiveCard(() => index);
+      }
+    });
+  });
+
+  const backgroundColors = ["#1F2937", "#000000", "#1F2937"];
+  const linearGradients = [
+    "linear-gradient(to bottom right, #06B6D4, #10B981)",
+    "linear-gradient(to bottom right, #F472B6, #8B5CF6)",
+    "linear-gradient(to bottom right, #F59E0B, #D97706)",
+  ];
+
+  return (
+    <motion.div
+      animate={{
+        backgroundColor: backgroundColors[activeCard % backgroundColors.length],
+      }}
+      className="h-[30rem] overflow-y-auto flex justify-center relative space-x-10 rounded-md p-10"
+      ref={ref}
+    >
+      <div className="div relative flex items-start px-4">
+        <div className="max-w-2xl">
+          {content.map((item, index) => (
+            <div key={item.title + index} className="my-20">
+              <motion.h2
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: activeCard === index ? 1 : 0.3,
+                }}
+                className="text-2xl font-bold text-slate-100"
+              >
+                {item.title}
+              </motion.h2>
+              <motion.p
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: activeCard === index ? 1 : 0.3,
+                }}
+                className="text-kg text-slate-300 max-w-sm mt-10"
+              >
+                {item.description}
+              </motion.p>
+            </div>
+          ))}
+          <div className="h-40" />
+        </div>
+      </div>
+      <motion.div
+        animate={{
+          backgroundImage: linearGradients[activeCard % linearGradients.length],
+        }}
+        className="hidden lg:block h-60 w-80 rounded-md bg-white sticky top-10 overflow-hidden"
+      ></motion.div>
+    </motion.div>
+  );
+};
