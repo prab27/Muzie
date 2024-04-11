a9bb02754742d652b744b8bfb69071588e125c31
prab27 <ayushsin72@gmail.com>
2024-04-12 00:48:14 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -11,6 +15,10 @@ export default function Home() {
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs/>
+      <MusicSchoolTestimonials/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
     </main>
 
            
